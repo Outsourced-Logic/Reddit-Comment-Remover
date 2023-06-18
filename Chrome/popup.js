f4eb1f4c62fdf659a804c833c4adaa994e518e6e
Outsourced-Logic <63562832+Outsourced-Logic@users.noreply.github.com>
2023-06-17 20:58:30 -0700

    Update popup.js

--- Chrome/popup.js
+++ Chrome/popup.js
@@ -1,21 +1,17 @@
-// The 'DOMContentLoaded' event fires when the initial HTML document has been completely loaded
-// This ensures that the HTML elements we want to interact with are available when the script runs
 document.addEventListener('DOMContentLoaded', function() {
-
-  // Get the checkbox element with the id 'toggle'
   let checkbox = document.getElementById('toggle');
-  
-  // Add an event listener for the 'change' event on the checkbox
-  // This event fires when the checkbox is checked or unchecked
+  let label = document.getElementById('status');
+
+  // Initialize label
+  label.innerText = checkbox.checked ? 'Enabled' : 'Disabled';
+
   checkbox.addEventListener('change', function() {
-    
-    // If the checkbox is checked...
+    // Change the label text whenever the checkbox state changes
+    label.innerText = checkbox.checked ? 'Enabled' : 'Disabled';
+
     if(checkbox.checked) {
-      // Query the active tab in the current window
+      // Execute the content script
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        
-        // Send a message to the background script with a command to start deleting
-        // Include the tab ID in the message for the background script to know where to execute the content script
         chrome.runtime.sendMessage({command: "startDeleting", tabId: tabs[0].id});
       });
     }
