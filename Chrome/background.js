20b3a5eee4ccd608ca8732b0e87a15b34a5f8803
Outsourced-Logic <63562832+Outsourced-Logic@users.noreply.github.com>
2023-06-23 11:48:28 -0700

    Update background.js
    This code waits for a toggle command, then executes the deleteComments.js script on the current active tab.

--- Chrome/background.js
+++ Chrome/background.js
@@ -1,26 +1,20 @@
-// Add an event listener for messages from the runtime
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    // Check if the received message has a command of "startDeleting"
-    if (request.command === "startDeleting") {
-      // Log a message with the tab ID where the command was sent from
-      console.log(`Received startDeleting command for tabId: ${request.tabId}`);
-      
-      // Execute the script "deleteComments.js" in the tab with the specified tab ID
-      chrome.scripting.executeScript({
-        // Specify the target tab by its ID
-        target: { tabId: request.tabId },
+    if (request.message === "toggle") {
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        let tab = tabs[0];
+        console.log(`Received toggle command for tabId: ${tab.id}`);
         
-        // The file(s) to execute. In this case, it's just "deleteComments.js"
-        files: ["deleteComments.js"]
-      }, () => { // This function will be called after executing the script
-        // If there was an error, log the error message
-        if (chrome.runtime.lastError) {
-          console.error(chrome.runtime.lastError.message);
-        } else { // If there was no error
-          // Log a success message
-          console.log('deleteComments.js injected successfully');
-        }
+        chrome.scripting.executeScript({
+          target: { tabId: tab.id },
+          files: ["deleteComments.js"]
+        }, () => {
+          if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError.message);
+          } else {
+            console.log('deleteComments.js injected successfully');
+          }
+        });
       });
     }
   }
