ac8df72391f90497649ed4dbcca9e93eeb0616cc
Outsourced-Logic <63562832+Outsourced-Logic@users.noreply.github.com>
2023-06-25 14:47:57 -0700

    Update whitelist.js
    This code will allow the user to add subreddit names to a whitelist. The whitelist is stored in the Chrome extension's local storage, so it persists across sessions. The user can also remove subreddit names from the whitelist.

--- Chrome/whitelist.js
+++ Chrome/whitelist.js
@@ -1,1 +1,52 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const subredditInput = document.querySelector('#subreddit-input');
+  const addButton = document.querySelector('#add-button');
+  const whitelist = document.querySelector('#whitelist');
 
+  // Load the existing whitelist from local storage
+  chrome.storage.local.get('whitelist', (data) => {
+    if (data.whitelist) {
+      for (const subreddit of data.whitelist) {
+        addSubredditToWhitelist(subreddit);
+      }
+    }
+  });
+
+  // Add the subreddit to the whitelist when the add button is clicked
+  addButton.addEventListener('click', () => {
+    const subreddit = subredditInput.value.trim();
+    if (subreddit) {
+      addSubredditToWhitelist(subreddit);
+
+      // Save the updated whitelist to local storage
+      chrome.storage.local.get('whitelist', (data) => {
+        const updatedWhitelist = data.whitelist ? [...data.whitelist, subreddit] : [subreddit];
+        chrome.storage.local.set({ whitelist: updatedWhitelist });
+      });
+
+      subredditInput.value = '';
+    }
+  });
+
+  function addSubredditToWhitelist(subreddit) {
+    const listItem = document.createElement('li');
+    listItem.textContent = subreddit;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Remove';
+    deleteButton.addEventListener('click', () => {
+      listItem.remove();
+
+      // Remove the subreddit from the whitelist in local storage
+      chrome.storage.local.get('whitelist', (data) => {
+        if (data.whitelist) {
+          const updatedWhitelist = data.whitelist.filter((item) => item !== subreddit);
+          chrome.storage.local.set({ whitelist: updatedWhitelist });
+        }
+      });
+    });
+
+    listItem.appendChild(deleteButton);
+    whitelist.appendChild(listItem);
+  }
+});
