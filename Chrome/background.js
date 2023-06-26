1fbfa1d1c8866bb287c38f1707473ea204c58b06
Outsourced-Logic <63562832+Outsourced-Logic@users.noreply.github.com>
2023-06-25 17:11:57 -0700

    Update background.js

--- Chrome/background.js
+++ Chrome/background.js
@@ -1,3 +1,5 @@
+let whitelist = [];
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if (request.message === "toggle") {
@@ -5,17 +7,39 @@ chrome.runtime.onMessage.addListener(
         let tab = tabs[0];
         console.log(`Received toggle command for tabId: ${tab.id}`);
         
-        chrome.scripting.executeScript({
-          target: { tabId: tab.id },
-          files: ["deleteComments.js"]
-        }, () => {
-          if (chrome.runtime.lastError) {
-            console.error(chrome.runtime.lastError.message);
-          } else {
-            console.log('deleteComments.js injected successfully');
-          }
-        });
+        let subreddit = tab.url.match(/reddit.com\/r\/([^/]*)/)[1];
+        if (!whitelist.includes(subreddit)) { 
+          chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            files: ["deleteComments.js"]
+          }, () => {
+            if (chrome.runtime.lastError) {
+              console.error(chrome.runtime.lastError.message);
+            } else {
+              console.log('deleteComments.js injected successfully');
+            }
+          });
+        } else {
+          console.log('This subreddit is whitelisted. Comments deletion skipped.');
+        }
       });
+    } else if (request.message === "add_to_whitelist") {
+      let subreddit = request.subreddit;
+      if (!whitelist.includes(subreddit)) {
+        whitelist.push(subreddit);
+        console.log(`Added ${subreddit} to whitelist.`);
+      } else {
+        console.log(`Subreddit ${subreddit} is already in the whitelist.`);
+      }
+    } else if (request.message === "remove_from_whitelist") {
+      let subreddit = request.subreddit;
+      let index = whitelist.indexOf(subreddit);
+      if (index !== -1) {
+        whitelist.splice(index, 1);
+        console.log(`Removed ${subreddit} from whitelist.`);
+      } else {
+        console.log(`Subreddit ${subreddit} is not in the whitelist.`);
+      }
     }
   }
 );
