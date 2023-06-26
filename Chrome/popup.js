ceb62b1babc3bd6f44a889c3a808b4b00a953d0d
Outsourced-Logic <63562832+Outsourced-Logic@users.noreply.github.com>
2023-06-25 17:15:18 -0700

    Update popup.js

--- Chrome/popup.js
+++ Chrome/popup.js
@@ -5,29 +5,42 @@ function updateStatusText() {
   status.textContent = isExtensionOn ? 'Enabled' : 'Disabled';
 }
 
+function updateWhitelist() {
+  chrome.runtime.sendMessage({ message: 'getWhitelist' }, function(response) {
+    document.querySelector('#whitelist').textContent = response.whitelist.join(', ');
+  });
+}
+
+function addSubreddit(event) {
+  event.preventDefault();
+  const subreddit = document.querySelector('#subreddit').value;
+  chrome.runtime.sendMessage({ message: 'addSubreddit', subreddit: subreddit }, function(response) {
+    updateWhitelist();
+    document.querySelector('#subreddit').value = '';
+  });
+}
+
+function removeSubreddit(event) {
+  event.preventDefault();
+  const subreddit = document.querySelector('#subredditRemove').value;
+  chrome.runtime.sendMessage({ message: 'removeSubreddit', subreddit: subreddit }, function(response) {
+    updateWhitelist();
+    document.querySelector('#subredditRemove').value = '';
+  });
+}
+
 function switchTabs(event) {
   const target = event.target;
   if (target.matches('#home-tab')) {
     document.querySelector('#home').classList.add('active');
     document.querySelector('#settings').classList.remove('active');
-    document.querySelector('#whitelist').classList.remove('active'); // Add this line
     target.classList.add('active');
     document.querySelector('#settings-tab').classList.remove('active');
-    document.querySelector('#whitelist-tab').classList.remove('active'); // And this one
   } else if (target.matches('#settings-tab')) {
     document.querySelector('#settings').classList.add('active');
     document.querySelector('#home').classList.remove('active');
-    document.querySelector('#whitelist').classList.remove('active'); // Add this line
     target.classList.add('active');
     document.querySelector('#home-tab').classList.remove('active');
-    document.querySelector('#whitelist-tab').classList.remove('active'); // And this one
-  } else if (target.matches('#whitelist-tab')) { // Add this entire block
-    document.querySelector('#whitelist').classList.add('active');
-    document.querySelector('#home').classList.remove('active');
-    document.querySelector('#settings').classList.remove('active');
-    target.classList.add('active');
-    document.querySelector('#home-tab').classList.remove('active');
-    document.querySelector('#settings-tab').classList.remove('active');
   }
 }
 
@@ -41,5 +54,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
   document.querySelector('.navbar').addEventListener('click', switchTabs);
 
+  const addForm = document.querySelector('#addForm');
+  addForm.addEventListener('submit', addSubreddit);
+
+  const removeForm = document.querySelector('#removeForm');
+  removeForm.addEventListener('submit', removeSubreddit);
+
   updateStatusText();
+  updateWhitelist();
 });
