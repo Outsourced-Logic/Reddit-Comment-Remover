d79d39bcf7f520cfae460a230106ee3edcf63159
Outsourced-Logic <63562832+Outsourced-Logic@users.noreply.github.com>
2023-06-25 17:11:07 -0700

    Update popup.js

--- Chrome/popup.js
+++ Chrome/popup.js
@@ -10,13 +10,24 @@ function switchTabs(event) {
   if (target.matches('#home-tab')) {
     document.querySelector('#home').classList.add('active');
     document.querySelector('#settings').classList.remove('active');
+    document.querySelector('#whitelist').classList.remove('active'); // Add this line
     target.classList.add('active');
     document.querySelector('#settings-tab').classList.remove('active');
+    document.querySelector('#whitelist-tab').classList.remove('active'); // And this one
   } else if (target.matches('#settings-tab')) {
     document.querySelector('#settings').classList.add('active');
     document.querySelector('#home').classList.remove('active');
+    document.querySelector('#whitelist').classList.remove('active'); // Add this line
     target.classList.add('active');
     document.querySelector('#home-tab').classList.remove('active');
+    document.querySelector('#whitelist-tab').classList.remove('active'); // And this one
+  } else if (target.matches('#whitelist-tab')) { // Add this entire block
+    document.querySelector('#whitelist').classList.add('active');
+    document.querySelector('#home').classList.remove('active');
+    document.querySelector('#settings').classList.remove('active');
+    target.classList.add('active');
+    document.querySelector('#home-tab').classList.remove('active');
+    document.querySelector('#settings-tab').classList.remove('active');
   }
 }
 
